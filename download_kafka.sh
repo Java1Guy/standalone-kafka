d55ed4d3b4aab65ea7f82d1a49dd10bf2dd6e51f
Paul Banks <banks@banksco.de>
2016-01-27 15:48:38 +0000

    Update Kafka version to 0.9.00..0, fix config so that it advertises localhost properly and works with docker-machine + port forwarding.

--- download_kafka.sh
+++ download_kafka.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-wget http://apache.fayea.com/kafka/0.8.2.1/kafka_2.9.2-0.8.2.1.tgz
+wget http://mirror.catn.com/pub/apache/kafka/0.9.0.0/kafka_2.11-0.9.0.0.tgz
 tar zxvf kafka_*.tgz
 rm kafka_*.tgz
+rm -rf kafka
 mv kafka_* kafka
+
+# Replace adevertised hostname with localhost in server config
+cat kafka/config/server.properties \
+    | sed 's/#advertised.host.name=<hostname routable by clients>/advertised.host.name=localhost/' \
+    > kafka/config/server-localhost.properties
